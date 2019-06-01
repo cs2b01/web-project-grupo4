1a067724292f43c039585a2d5cb6505c6523c8c4
THEFLILUX <jorge.nicho@utec.edu.pe>
2019-05-31 20:09:42 -0500

    Marcadores y estilos del login

--- web/server.py
+++ web/server.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/mapa')
+def mapa():
+    return render_template('mapa.html')
+
 if __name__ == '__main__':
     app.run()
